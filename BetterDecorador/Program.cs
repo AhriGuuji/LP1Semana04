8a04452d7bf515c27d6ec806683c74eedfaa77f3
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-12 14:48:43 +0000

    Add BetterDecorador

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor(args[0],char.Parse(args[1]),int.Parse(args[2])));
+        }
+
+        private static string Decor(string x, char y, int z)
+        {
+
+            for (int i = 0; i < z; i++)
+            {
+                characters += y;
+            }
+            return $"{characters} {x} {characters}";
+        }
+    }
+}

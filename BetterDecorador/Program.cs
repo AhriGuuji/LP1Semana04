53b78f1fccbeedcb08f5080e78f90e13b87b7712
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-03-12 15:12:52 +0000

    Added comments in XML to BetterDecorador

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -4,19 +4,30 @@ namespace BetterDecorador
 {
     public class Program
     {
+        /// <summary>
+        /// The launcher to start commands on the system.
+        /// </summary>
+        /// <param name="args">The arguments to use in certain parameters</param>
         private static void Main(string[] args)
         {
             Console.WriteLine(Decor(args[0],char.Parse(args[1]),int.Parse(args[2])));
         }
 
-        private static string Decor(string x, char y, int z)
+/// <summary>
+/// Create a decorated string.
+/// </summary>
+/// <param name="phrase">The phrase in the middle.</param>
+/// <param name="decoration">The decoration char you want on each side.</param>
+/// <param name="quantity">How many decoration char you want.</param>
+/// <returns>Returns the decorated string to use on main</returns>
+        private static string Decor(string phrase, char decoration, int quantity)
         {
             string characters = "";
-            for (int i = 0; i < z; i++)
+            for (int i = 0; i < quantity; i++)
             {
-                characters += y;
+                characters += decoration;
             }
-            return $"{characters} {x} {characters}";
+            return $"{characters} {phrase} {characters}";
         }
     }
 }
